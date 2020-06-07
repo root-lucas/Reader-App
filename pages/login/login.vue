22a6d9e34f039670f2758d562da38cd7cf185a98
root-lucas <945832032@qq.com>
2020-06-08 02:53:41 +0800

    feat: API security sign

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -8,6 +8,7 @@
 
 <script>
 	let _self, pageOptions, session_key, openid;;
+	let sign = require('../../commons/sign.js');
 	export default {
 		data() {
 			return {
@@ -19,6 +20,8 @@
 			// #ifdef MP-WEIXIN
 			getUserInfo:function(info){
 				info = info.mp.detail.userInfo;
+				// 获取本地缓存的签名并让服务器验证签名
+				let sign = uni.getStorageSync('sign');
 				uni.request({
 					url: _self.apiServer+'member&m=login',
 					method: 'POST',
@@ -27,6 +30,7 @@
 						openid : openid,
 						name   : info.nickName,
 						face   : info.avatarUrl,
+						sign   : sign
 					},
 					success:(res)=>{
 						// console.log('res = ',res);
@@ -60,6 +64,8 @@
 		onLoad: function(options){
 			_self = this;
 			pageOptions = options;
+			// 接口安全———生成签名,并缓存到了本地
+			sign.sign(this.apiServer);
 			
 			// 微信小程序端使用微信授权登录
 			// #ifdef MP-WEIXIN
@@ -72,7 +78,7 @@
 						url:_self.apiServer+'member&m=codeToSession&code='+res.code,
 						success: (sessions) => {
 							// console.log('sessions = ', sessions)
-							// 获取 unionId
+							// 获取 unionId,session_key用于 unionId应用
 							session_key = sessions.data.session_key;
 							openid      = sessions.data.openid;
 						},
@@ -90,7 +96,8 @@
 					uni.getUserInfo({
 						provider: 'weixin',  // 不填依旧默认是微信
 						success:function(info){
-							console.log('请求登录授权成功的用户全部信息 = ', JSON.stringify(info))
+							let sign = uni.getStorageSync('sign');
+							// console.log('请求登录授权成功的用户全部信息 = ', JSON.stringify(info))
 							
 							// 将用户登录授权的信息保存至后端数据库
 							uni.request({
@@ -102,10 +109,11 @@
 									openid: info.userInfo.openId,
 									name: info.userInfo.nickName,
 									face: info.userInfo.avatarUrl,
+									sign: sign
 								},
 								success:res=>{
 									// 前面保存成功后再从后端数据库读取信息
-									console.log("成功获取后端数据库信息 = ", JSON.stringify(res))
+									// console.log("成功获取后端数据库信息 = ", JSON.stringify(res))
 									res = res.data;
 									// 登录成功 记录会员信息到本地缓存
 									if(res.status == 'ok'){
@@ -126,7 +134,7 @@
 									}
 								},
 								fail:(e)=>{
-									console.log("获取后端数据库信息失败 = ",JSON.stringify(e))
+									// console.log("获取后端数据库信息失败 = ",JSON.stringify(e))
 									uni.showToast({title:"微信登录授权失败"});
 								}
 							})
