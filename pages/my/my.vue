d44dafae77ca87927d220bcf916d9bf2a9d058c0
root-lucas <945832032@qq.com>
2020-06-08 20:34:05 +0800

    feat: My page layout and Get user info

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,27 +1,117 @@
 <template>
-	<view>
-		hello my page
-	</view>
+    <view class="grace-padding">
+        <view class="myface"><image :src="myFace" mode="widthFix"></image></view>
+        <view class="grace-box-banner" style="margin:30rpx 0;">
+            <view class="garce-items">
+                <view class="line1">{{user.artCount}}</text></view>
+                <view class="line2">文章</view>
+            </view>
+            <view class="garce-items">
+                <view class="line1">{{user.u_integral}}</text></view>
+                <view class="line2">积分</view>
+            </view>
+            <view class="garce-items">
+                <view class="line1">{{user.u_remainder}}</text></view>
+                <view class="line2">余额</view>
+            </view>
+            <view class="garce-items">
+                <view class="line1">0</text></view>
+                <view class="line2">消息</view>
+            </view>
+        </view>
+        <view class="grace-title grace-nowrap grace-space-between">
+            <view class="grace-h5 grace-blod">我的文章</view>
+        </view>
+        <view class="myart-list" v-for="(item, index) in arts" :key="index">
+            <view class="title">{{item.art_title}}</view>
+            <view class="btns">
+                <view :data-artid="item.art_id"  @tap="editArt">编辑</view>
+                <view :data-artid="item.art_id" :data-index="index" @tap="removeArt">删除</view>
+            </view>
+        </view>
+        <view class="loadMore" @tap="getArtList">{{loadMore}}</view>
+    </view>
 </template>
 
 <script>
-	let loginRes
+	let loginRes, _self, page = 1;						
 	export default {
 		data() {
 			return {
-				
+				myFace : '',
+				arts : [],
+				loadMore : "点击加载更多",
+				user : []
 			}
 		},
 		onLoad:function() {
+			_self = this;
 			// 使用 main.js 定义的全局方法来检查用户是否登录
 			loginRes = this.checkLogin('./my/my',2)
 			// console.log('loginRes = ',loginRes)
 			// 尚未登陆则跳转至login.vue
 			if(!loginRes){return;}
+			this.myFace = loginRes[3];
+		},
+		onShow:function(){
+			//加载我的文章
+			this.arts = [];
+			page = 1;
+			this.getArtList();
+			// 加载会员信息
+			uni.request({
+				url: this.apiServer + 'my&m=info',
+				method: 'POST',
+				header: {'content-type' : "application/x-www-form-urlencoded"},
+				data: {
+					uid : loginRes[0],
+					random : loginRes[1]
+				},
+				success: res => {
+					console.log(res);
+					if(res.data.status == 'ok'){
+						this.user = res.data.data;
+					}
+				}
+			});
+		},
+		methods:{
+			getArtList : function(){
+				if(this.loadMore != '点击加载更多'){return ;}
+				this.loadMore = '加载中...';
+				uni.request({
+					url: this.apiServer + 'my&m=arts&page='+page,
+					method: 'POST',
+					header: {'content-type' : "application/x-www-form-urlencoded"},
+					data: {
+						uid : loginRes[0],
+						random : loginRes[1]
+					},
+					success: res => {
+						console.log(res);
+						if(res.data.status == 'ok'){
+							this.arts = this.arts.concat(res.data.data);
+							page++;
+							this.loadMore = '点击加载更多';
+						}else if(res.data.status == 'empty'){
+							this.loadMore = '已经加载全部';
+						}else{
+							this.loadMore = '点击加载更多';
+						}
+					}
+				})
+			},
 		}
 	}
 </script>
 
 <style>
-
+.myface{width:88px; height:88px; border:5px solid #F1F2F3; border-radius:100%; margin:15px auto;}
+.myface image{width:100%; border-radius:100%;}
+.myart-list{width:100%; margin:8px 0; overflow:hidden; border-bottom:1px dashed #D7D8D9;}
+.myart-list .title{line-height:2em; width:100%;}
+.myart-list .btns{line-height:2em; width:100%;}
+.myart-list .btns view{float:right; padding:0 6px; margin:0 5px; color:#00B26A;}
+.myart-list .btns view:last-child{color:#F76260;}
+.loadMore{width:100%; padding:8px 0; text-align:center; color:#CCCCCC;}
 </style>
